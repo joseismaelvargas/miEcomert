4d112abdf795d9ddbd2ffbd68eb387382a91a7b2
ismael <joseismaelvargas@users.noreply.github.com>
2025-07-15 22:11:34 -0300

    multer

--- src/routes/producto.routes.js
+++ src/routes/producto.routes.js
@@ -1,10 +1,19 @@
 import { Router } from "express";
-
+import { subirImage } from "../controllers/multer.js";
 import { funcionprueba,verProducto,editProducto,crearProducto,deleteProducto} from "../controllers/productoscontrolers.js";
-
+import { saveImage } from "../controllers/multer.js";
 const ruta=Router()
 
 ruta.route("/producto").post(crearProducto).get(verProducto)
 ruta.route("/producto/:id").put(editProducto).delete(deleteProducto)
-
+ruta.route('/images/single').post(subirImage,(req,res)=>{
+    res.send('Imagen cargado con exito');
+     console.log(req.file);
+   saveImage(req.file)
+   res.json({
+  mensaje: 'Imagen subida correctamente',
+  archivo: req.file,
+  url: `/uploads/${req.file.filename}`
+});
+})
 export default ruta
